c23a9d316e993b47e0de78e2968952757ed9bb86
SivaSurender <git@stackblitz.com>
2025-03-07 17:16:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-zjblydhv
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/SivaSurender/vitejs-vite-zjblydhv)
---
+++ packages/brandA/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  source: ['../base/tokens/token.json', 'tokens/token.json'],
+  platforms: {
+    css: {
+      transformGroup: 'css',
+      buildPath: 'build/css/',
+      files: [
+        {
+          destination: 'variables.css',
+          format: 'css/variables',
+        },
+      ],
+    },
+  },
+};
